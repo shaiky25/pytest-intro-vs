cb7e527ac94c77ab4e6e22c2a160ac9db37a71ed
Faiz <37562686+shaiky25@users.noreply.github.com>
2025-02-10 21:02:52 -0600

    Update test_Calc.py

--- test_Calc.py
+++ test_Calc.py
@@ -4,7 +4,7 @@ def test_add():
     assert add(2,3) == 5
 
 def test_subtract():
-    assert subtract(2, 3) == -1
+    assert subtract(2, 3) == 1
 
 def test_multiply():
     assert multiply(2, 3) == 6
